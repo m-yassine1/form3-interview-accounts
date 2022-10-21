03e76219c3ce8b555e3fa97f83b009c5b006f8fd
mohamadyassine <muhamed.yassine@gmail.com>
2022-10-21 09:20:02 +0300

    Fixed test to pass filter parameter

--- account/api/account_api_test.go
+++ account/api/account_api_test.go
@@ -30,7 +30,7 @@ func TestGetAccounts(t *testing.T) {
 	defer httpmock.DeactivateAndReset()
 	httpmock.RegisterResponder("GET", fmt.Sprintf("%s/v1/organisation/accounts", hostname),
 		httpmock.NewStringResponder(200, `{     "data": [         {             "attributes": {                 "alternative_names": null,                 "country": "GB",                 "name": []             },             "created_on": "2022-10-19T09:03:08.334Z",             "id": "0d209d7f-d07a-4542-947f-5885fddddae7",             "modified_on": "2022-10-19T09:03:08.334Z",             "organisation_id": "ba61483c-d5c5-4f50-ae81-6b8c039bea43",             "type": "accounts",             "version": 0         },         {             "attributes": {                 "alternative_names": null,                 "country": "GB",                 "name": []             },             "created_on": "2022-10-19T09:05:40.235Z",             "id": "0d209d7f-d07a-4542-947f-5885fddddae8",             "modified_on": "2022-10-19T09:05:40.235Z",             "organisation_id": "ba61483c-d5c5-4f50-ae81-6b8c039bea43",             "type": "accounts",             "version": 0         }] }]`))
-	accounts, err := accountApi.GetAccounts()
+	accounts, err := accountApi.GetAccounts(nil)
 	assert.Empty(t, err, "Error is not empty")
 	assert.NotEmpty(t, accounts, "Accounts list is empty")
 	assert.Equal(t, len(accounts), 2)
